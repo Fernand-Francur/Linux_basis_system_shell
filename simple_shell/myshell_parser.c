0e9397ebe266fd891c8d389f5af7034e0c79279a
Fernand-Francur <timobohr@bu.edu>
2022-01-29 19:26:16 +0000

    Trying Regular expressions

--- simple_shell/myshell_parser.c
+++ simple_shell/myshell_parser.c
@@ -4,12 +4,23 @@
 #include "regex.h"
 #include "stdio.h"
 #include "string.h"
+#include <stdlib.h>
 
 struct pipeline *pipeline_build(const char *command_line)
 {
   regex_t regex;
   int regexVal;
+
+  regexVal = regcomp(&regex, "[ \t\n]*([\w\-\.]+)[ \t\n]*[\|><&]?", 0);
+  if (regexVal) {
+    fprintf(stderr, "Compilation failed\n");
+    exit(1);
+  }
+
+  regexVal = regexec(&regex, command_line, 0, NULL, 0);
+
   
+  printf("%s", command_line);
 
   // TODO: Implement this function
 	return NULL;
@@ -19,9 +30,13 @@ void pipeline_free(struct pipeline *pipeline)
 {
 	// TODO: Implement this function
 }
-//int main() {
-//  printf("Hello\n");
-//  printf("Testing\n");
-//  return 0;
-//}
+
+int main() {
+  const char* com = "ls|wc -l >counts.txt\n";
+  struct pipeline commandtest;
+  commandtest = *pipeline_build(com);
+  printf("Hello\n");
+  printf("Testing\n");
+  return 0;
+}
   
